c1a8f58a23597cd63f595e5597f8bbca91bad609
Kimsu <betnwjd25@gmail.com>
2023-07-19 00:06:46 +0900

    ADD: 카카오로그인 및 추가정보회원가입 통신완료

---
+++ src/pages/Login/Oauth.js
@@ -0,0 +1,27 @@
+/*global Kakao*/
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Oauth = () => {
+  const location = useLocation();
+  const CODE = location.search.split('=')[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`http://10.58.52.101:3000/users/signin?code=${CODE}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data && data.accessToken.newUser === false) {
+          localStorage.setItem('token', data.accessToken.accessToken);
+          navigate('/');
+        } else {
+          localStorage.setItem('token', data.accessToken.accessToken);
+          navigate('/signup');
+        }
+      });
+  }, []);
+
+  return <></>;
+};
+
+export default Oauth;
