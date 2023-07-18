eca97dc11df5a00ff79877af56601695e7fa47fe
Kimsu <betnwjd25@gmail.com>
2023-07-18 10:42:27 +0900

    ADD: FOOTER 레이아웃

---
+++ src/components/Footer/FooterList.js
@@ -0,0 +1,22 @@
+const FOOTER_LIST = [
+  {
+    id: 1,
+    title: '가보자GO',
+    list: [
+      { id: 1, listTitle: '이용약관', path: '/' },
+      { id: 2, listTitle: '개인정보처리방침', path: '/' },
+      { id: 3, listTitle: '위치기반 서비스 이용약관', path: '/' },
+      { id: 4, listTitle: '데이터 제휴 문의', path: '/' },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Account',
+    list: [
+      { id: 1, listTitle: '로그인', path: '/login' },
+      { id: 2, listTitle: '메인페이지', path: '/' },
+    ],
+  },
+];
+
+export default FOOTER_LIST;
---
+++ src/components/Footer/FooterMembers.js
@@ -0,0 +1,27 @@
+const FOOTER_MEMBER = [
+  {
+    id: 1,
+    name: 'FRONT',
+    img: 'images/sujeong.png',
+    github: 'https://github.com/Kimsu10',
+  },
+  {
+    id: 2,
+    name: 'BACK',
+    img: 'images/donghee.png',
+    github: 'https://github.com/donghee9',
+  },
+  {
+    id: 3,
+    name: 'BACK',
+    img: 'images/juhyeon.png',
+    github: 'httpshttps://github.com/sioscorial',
+  },
+  {
+    id: 4,
+    name: 'BACK',
+    img: 'images/junsu.png',
+    github: 'https://github.com/joonsooha123',
+  },
+];
+export default FOOTER_MEMBER;
